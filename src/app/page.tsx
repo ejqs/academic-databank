2641d5052e5c2d33d3823bec28b7f210cd77d43d
earlsab <62688314+earlsab@users.noreply.github.com>
2024-12-17 12:26:06 +0800

    Fix no session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
-  if (!session.user)
+  if (!session?.user)
     return (
       <div>
         {strings.homepageAcessible}
