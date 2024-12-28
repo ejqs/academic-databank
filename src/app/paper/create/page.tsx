61065a5a09d90cdeb0376d704b5f2213850eb2b2
earlsab <62688314+earlsab@users.noreply.github.com>
2024-12-29 00:27:41 +0800

    Working edit paper

--- src/app/paper/create/page.tsx
+++ src/app/paper/create/page.tsx
@@ -1,22 +1,23 @@
 // import Form from "@components/Form/Form";
 
-import { AddForm } from "@/features/papers/components/forms/AddForm";
+import { PaperForm } from "@/features/papers/components/forms/PaperForm";
 import { DefaultTags } from "@/features/papers/server/enums";
 
 export default function CreatePaper() {
   // Initial form data for creating a new paper
 
   //TODO: Allow fetching the most commonly used tags from database
-  const tags = Object.values(DefaultTags).map((tags) => ({
-    value: tags,
-    label: tags,
-  }));
 
+  // Tags are processed like
+  // const DefaultTags = Object.values(Tags).map((tags) => ({
+  //   value: tags,
+  //   label: tags,
+  // }));
   // Useful Reference: https://www.youtube.com/watch?v=dDpZfOQBMaU
   return (
     <div>
       <h1>Create a New Paper</h1>
-      <AddForm Tags={tags}></AddForm>
+      <PaperForm Tags={DefaultTags} existingPaper={undefined}></PaperForm>
     </div>
   );
 }
---
+++ src/components/Breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,3 @@
+export default function Breadcrumbs({ breadcrumbs }) {
+  // Recieve and array
+}
